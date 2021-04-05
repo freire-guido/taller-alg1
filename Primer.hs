000b9e5574e916faf35a1d6546727a125e3b023c
freire-guido <freireguido@outlook.com>
2021-04-05 19:07:06 -0300

    clase 3 pt 1

--- Primer.hs
+++ Primer.hs
@@ -10,3 +10,20 @@ distanciaPuntos v w = sqrt ((fst v - fst w)^2 + (snd v - snd w)^2)
 
 crearPar :: a -> b -> (a, b)
 crearPar x y = (x, y)
+
+-- Clase 3
+fib :: Int -> Int
+fib n | n > 1 = fib(n-1) + fib(n-2)
+      | otherwise = n
+
+parteEntera :: Float -> Int 
+parteEntera n | 0 <= n && n < 1 = 0
+              | otherwise = parteEntera (n - 1) + 1
+
+esMultiploTres :: Int -> Bool
+esMultiploTres n | n - 3 == 0 = True
+                 | n - 3 < 0 = False
+                 | otherwise = esMultiploTres (n -3)
+
+sumaImpares :: Int -> Int
+sumaImpares n = n^2 --Como seria recursivamente?
