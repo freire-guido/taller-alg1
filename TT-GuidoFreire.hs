058f67ad3d74628ac4e9936ce2fed76f6968410b
freire-guido <freireguido@outlook.com>
2021-04-28 16:33:09 -0300

    diferencia

--- TT-GuidoFreire.hs
+++ TT-GuidoFreire.hs
@@ -10,17 +10,16 @@ TURNO TARDE
 
 -- EJERCICIO 1: Recibe un numero natural n y devuelve True si y solo si el n es par, mayor que 2 y suma de dos numeros primos o False en caso contrario.
 satisfaceGoldbach :: Integer -> Bool
-satisfaceGoldbach = satisfaceGoldbachPreciso (1, 1)
+satisfaceGoldbach n = satisfaceGoldbachDesde n (div n 2)
 
 -- Recibe tres numeros naturales i j n y devuelve True si y solo si el n es par, mayor que 2 y suma del i y j-esimos numeros primos.
 -- Intenta con el siguiente i-esimo numero primo en caso contrario, hasta que el mismo es igual al j-esimo numero primo.
 -- En ese caso intenta con el siguiente j-esimo numero primo y empieza a contar de nuevo con el i.
-satisfaceGoldbachPreciso :: (Integer, Integer) -> Integer -> Bool
-satisfaceGoldbachPreciso (i, j) n
-  | n <= 2 || mod n 2 /= 0 = False
-  | nEsimoPrimo i + nEsimoPrimo j == n = True
-  | i == j = satisfaceGoldbachPreciso (1, j + 1) n
-  | otherwise = satisfaceGoldbachPreciso (i + 1, j) n
+satisfaceGoldbachDesde :: Integer -> Integer -> Bool
+satisfaceGoldbachDesde n k
+  | n <= 2 || odd n = False
+  | esPrimo (n - minimoPrimoDesde k) = True
+  | otherwise = satisfaceGoldbachDesde n (k + 1)
 
 -- EJERCICIO 2: Recibe un numero natural n par mayor que 2 y devuelve True si y solo si la conjetura es cierta para todos los naturales pares mayores que 2 y menores o iguales que n o False en caso contrario.
 verificarConjeturaHasta :: Integer -> Bool
@@ -41,9 +40,10 @@ dEPD (i, j) n
   | i == j = dEPD (1, j + 1) n
   | otherwise = dEPD (i + 1, j) n
 
+{-
 -- EJERCICIO 4: Recibe un numero natural n par mayor que 2 y devuelve la cantidad de pares ordenados (a, b) de numeros primos tales que a + b == n
-{-numeroDeDescomposiciones :: Integer -> Integer
-numeroDeDescomposiciones = numeroDeDescomposicionesDesde (2, 2)-}
+numeroDeDescomposiciones :: Integer -> Integer
+numeroDeDescomposiciones = numeroDeDescomposicionesDesde (2, 2)
 
 -- nDD usa numeros primos en vez de posiciones i / j de numeros primos, ahorra calculo de nEsimosPrimos y ademas puede empezar a contar primos desde j=n/2
 nDD :: Integer -> Integer
@@ -54,24 +54,20 @@ nDDD (i, j) n
   | j == 0 = 0
   | i == j = 1 + nDDD (dEPD (i, j) n) n
   | otherwise = 2 + nDDD (dEPD (i, j) n) n
-
+-}
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Las funciones de aca en adelante se utilizan para encontrar el n-esimo numero primo (Clase 5)
-menorDivisorDesde :: Integer -> Integer -> Integer
-menorDivisorDesde n k
-  | mod n k == 0 = k
-  | otherwise = menorDivisorDesde n (k + 1)
-
-menorDivisor :: Integer -> Integer
-menorDivisor n = menorDivisorDesde n 2
+-- Las funciones de aca en adelante se utilizan para encontrar el minimo primo desde un numero (Clase 5)
+esPrimoDesde :: Integer -> Integer -> Bool
+esPrimoDesde n k
+  | mod n k == 0 = False
+  | k > div n 2 = True
+  | otherwise = esPrimoDesde n (k + 1)
 
 esPrimo :: Integer -> Bool
-esPrimo n = menorDivisor n == n
-
-nEsimoPrimo :: Integer -> Integer
-nEsimoPrimo 1 = 2
-nEsimoPrimo n = minimoPrimoDesde (1 + nEsimoPrimo (n -1))
+esPrimo 1 = False
+esPrimo 2 = True
+esPrimo n = esPrimoDesde n 2
 
 minimoPrimoDesde :: Integer -> Integer
 minimoPrimoDesde n
