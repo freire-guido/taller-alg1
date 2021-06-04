69784fcf930dda90335be02e040df55030f1c7ba
freire-guido <freireguido@outlook.com>
2021-06-04 14:44:44 -0300

    pattern match listas

--- Segundo/TT-GuidoFreire.hs
+++ Segundo/TT-GuidoFreire.hs
@@ -22,7 +22,7 @@ posiblesJugadasEn i x = (i, x) : posiblesJugadasEn i (x -1)
 
 unirJugadas :: [Jugada] -> [Jugada] -> [Jugada]
 unirJugadas [] t = t
-unirJugadas s t = unirJugadas (tail s) (head s : t)
+unirJugadas (s:st) t = unirJugadas st (s : t)
 
 -- EJERCICIO 3: Decide si una posición p es ganadora.
 esPosicionGanadora :: Posicion -> Bool
@@ -32,16 +32,16 @@ hayJugadaGanadora :: Posicion -> [Jugada] -> Bool
 hayJugadaGanadora _ [] = False
 hayJugadaGanadora [] _ = False
 hayJugadaGanadora [1] _ = True
-hayJugadaGanadora p j = not (esPosicionGanadora (jugar p (head j))) || hayJugadaGanadora p (tail j)
+hayJugadaGanadora p (j:js) = not (esPosicionGanadora (jugar p j)) || hayJugadaGanadora p js
 
 -- EJERCICIO 4: Recibe una posición ganadora p y devuelve una jugada que dejaría al rival en una posición no ganadora.
 jugadaGanadora :: Posicion -> Jugada
 jugadaGanadora p = cualJugadaGanadora p (posiblesJugadas p)
 
 cualJugadaGanadora :: Posicion -> [Jugada] -> Jugada
-cualJugadaGanadora p j
-  | not (esPosicionGanadora (jugar p (head j))) = head j
-  | otherwise = cualJugadaGanadora p (tail j)
+cualJugadaGanadora p (j:js)
+  | not (esPosicionGanadora (jugar p j)) = j
+  | otherwise = cualJugadaGanadora p js
 
 -- EJERCICIO 5: Recibe una posición p (no necesariamente ganadora) y devuelve la cantidad de jugadas ganadoras partiendo de p.
 numeroDeJugadasGanadoras :: Posicion -> Int
@@ -49,6 +49,6 @@ numeroDeJugadasGanadoras p = cuantasJugadasGanadoras p (posiblesJugadas p)
 
 cuantasJugadasGanadoras :: Posicion -> [Jugada] -> Int
 cuantasJugadasGanadoras p [] = 0
-cuantasJugadasGanadoras p j
-  | not (esPosicionGanadora (jugar p (head j))) = 1 + cuantasJugadasGanadoras p (tail j)
-  | otherwise = cuantasJugadasGanadoras p (tail j)
+cuantasJugadasGanadoras p (j:js)
+  | not (esPosicionGanadora (jugar p j)) = 1 + cuantasJugadasGanadoras p js
+  | otherwise = cuantasJugadasGanadoras p js
